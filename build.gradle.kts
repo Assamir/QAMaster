60f10f0c23855fb190c478c9d7608a828842ec74
GosiaGlebowicz <gosia@kimputing.com>
2023-05-28 09:44:09 +0200

    get

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("io.github.bonigarcia", "webdrivermanager", "5.3.2")
     implementation("io.rest-assured", "rest-assured", "5.3.0")
     implementation("org.seleniumhq.selenium", "selenium-java", "4.9.1")
+    implementation("org.slf4j:slf4j-api:2.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
 
