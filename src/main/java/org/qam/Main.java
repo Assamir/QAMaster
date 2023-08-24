0403bc29acd77b778ae7be3f81f320feb8242e2e
Assamir <dariusz.czajkiewicz@gmail.com>
2023-08-24 19:37:14 +0200

    QAM-22

--- src/main/java/org/qam/Main.java
+++ src/main/java/org/qam/Main.java
@@ -2,11 +2,15 @@ package org.qam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableJpaRepositories(basePackages = {"org.qam.service"})
 @EnableJpaRepositories(basePackages = {"org.qam.repository"})
-@SpringBootApplication()
+//@SpringBootApplication()
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JdbcTemplateAutoConfiguration.class })
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
