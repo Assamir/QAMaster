f22243f2fdc9ebe7d2ee57363188275ffd64ffab
Assamir <dariusz.czajkiewicz@gmail.com>
2024-11-19 19:51:25 +0100

    QAM-22

--- src/test/java/amazon/BasicTest.java
+++ src/test/java/amazon/BasicTest.java
@@ -33,16 +33,4 @@ class BasicTest {
         testContext.finish();
         testContext.addStep("Finished UI part", true);
     }
-//    @TestConfiguration(testPriority = TestPriority.HIGH, testTypes = {TestType.API})
-//    void basicApiSearch() {
-//        var testContext = scenarioContext.createTestContext();
-//
-//        var planets = testContext.getApiManager().Planets().getPlanets();
-//        logger.info("planets.size: " + planets.size());
-//        logger.info("planets" + planets.toString());
-//        for (var planet : planets) {
-//            logger.info("planet.name: " + planet.getName());
-//            logger.info("planet.url: " + planet.getUrl());
-//        }
-//    }
 }
